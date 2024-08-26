7445c73664cfb43831e658ec2fcad2c517577a5b
Lydia Owens <owens23a@unc.edu>
2024-08-26 09:36:34 -0400

    Functions from 8/26

---
+++ lessons/functions.py
@@ -0,0 +1,14 @@
+"""Practice with functions"""
+
+from random import randint
+
+# Defining a function
+
+
+def sum(num1: int, num2: int) -> int:
+    """To sum 2 numbers together and return num 1 + num 2"""
+    return num1 + num2
+
+
+# call the function
+print(sum(num1=30, num2=8))
