6c04bc12dfe825187908d3454e8e3a4907d82cd8
Lydia Owens <owens23a@unc.edu>
2024-08-28 09:21:20 -0400

    CQ00-Functions

---
+++ .devcontainer/CQs/cq00_functions.py
@@ -0,0 +1,8 @@
+""" Challenge Question 00 for COMP 110"""
+
+__author__ = "730671309"
+
+
+def mimic(message: str) -> str:
+    """Will take the input and repeat it back as a string"""
+    return message
