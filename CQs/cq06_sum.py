8ce4a9141b3309278fb1e514f2d4071ece26e722
Lydia Owens <owens23a@unc.edu>
2024-10-16 23:40:07 -0400

    CQ06

--- CQs/cq06_sum.py
+++ CQs/cq06_sum.py
@@ -21,4 +21,25 @@ print(w_sum(vals=[1.1, 0.9, 1.0]))
 
 # part 2. f_sum()
 def f_sum(vals: list[float]) -> float:
+    if len(vals) == 0:
+        return 0.0
     sum: float = 0.0
+    for value in vals:
+        sum += value
+    return sum
+
+
+print(f_sum(vals=[1.1, 0.9, 1.0]))
+
+
+# part 3. r_range_sum()
+def f_range_sum(vals: list[float]) -> float:
+    if len(vals) == 0:
+        return 0.0
+    sum: float = 0.0
+    for index in range(0, len(vals)):
+        sum += vals[index]
+    return sum
+
+
+print(f_range_sum(vals=[1.1, 0.9, 1.0]))
